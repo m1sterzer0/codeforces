93e716499cbc9424b8d83ce7982ff82b52b5d73f
David Bradley <debr5836@gmail.com>
2019-02-25 16:40:03 -0700

    cpalgorithm note update

---
+++ lib/cpalgorithms_linearalgebra.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint64_t ull;
+typedef int64_t  ll;
+const ll infll = 9223372036854775807LL;
+#define PB push_back  
+#define MP make_pair  
+#define SZ(v) ((ll)(v).size())  
+#define FOR(i,a,b) for(ll i=(a);i<(b);++i)  
+#define REP(i,n) FOR(i,0,n)  
+#define FORE(i,a,b) for(ll i=(a);i<=(b);++i)  
+#define REPE(i,n) FORE(i,0,n)  
+#define FORSZ(i,a,v) FOR(i,a,SZ(v))  
+#define REPSZ(i,v) REP(i,SZ(v))
+
+//////////////////////////////////////////////////
+//
+// SECTION A: MATRICES
+//
+//////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A1: GAUSS & SYSTEM OF LINEAR EQUATIONS
+// https://cp-algorithms.com/linear_algebra/linear-system-gauss.html
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A2: GAUSS & DETERMINANT
+// https://cp-algorithms.com/linear_algebra/determinant-gauss.html
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A3: KRAUT & DETERMINANT
+// https://cp-algorithms.com/linear_algebra/determinant-kraut.html
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A4: RANK OF A MATRIX
+// https://cp-algorithms.com/linear_algebra/rank-matrix.html
+////////////////////////////////////////////////////////////
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(15);
+    //freopen("999A.in1","r",stdin);
+    return 0;
+}
---
+++ lib/cpalgorithms_numericalmethods.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint64_t ull;
+typedef int64_t  ll;
+const ll infll = 9223372036854775807LL;
+#define PB push_back  
+#define MP make_pair  
+#define SZ(v) ((ll)(v).size())  
+#define FOR(i,a,b) for(ll i=(a);i<(b);++i)  
+#define REP(i,n) FOR(i,0,n)  
+#define FORE(i,a,b) for(ll i=(a);i<=(b);++i)  
+#define REPE(i,n) FORE(i,0,n)  
+#define FORSZ(i,a,v) FOR(i,a,SZ(v))  
+#define REPSZ(i,v) REP(i,SZ(v))
+
+//////////////////////////////////////////////////
+//
+// SECTION A: SEARCH
+//
+//////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A1: Ternary Search
+// https://cp-algorithms.com/num_methods/ternary_search.html
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION A2: Newton's method for finding roots
+// https://cp-algorithms.com/num_methods/roots_newton.html
+////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+//
+// SECTION B: INTEGRATION
+//
+//////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// SUBSECTION B1: Integration by Simpson's formula
+// https://cp-algorithms.com/num_methods/simpson-integration.html
+////////////////////////////////////////////////////////////
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cout << fixed << setprecision(15);
+    //freopen("999A.in1","r",stdin);
+    return 0;
+}
